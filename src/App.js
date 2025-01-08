2d13c816e29fcb8cf435f875606e8a7348c8832e
Arsalan Bashir <arsalanbashir29@gmail.com>
2025-01-08 16:58:12 +0530

    Initial Upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/Home'
+import './style.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='heading'>TO DO LIST</h1>
+      <Home />
+    </>
+  );
+}
+
+export default App;
